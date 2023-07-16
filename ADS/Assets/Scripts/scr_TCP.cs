670104888f3f8ffc5728993f1b23d51f0a658d76
Chilik78 <104494266+Chilik78@users.noreply.github.com>
2023-07-16 04:08:10 +0300

    Коннект, Дзержинский, Траффик в Юнити
    Перенесен коннект. Добавлен Дзержинский район и +- соединен как в SUMO. Траффик работает в Unity

---
+++ ADS/Assets/Scripts/scr_TCP.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class scr_TCP : MonoBehaviour
+{
+    private TcpClient socketConnection; //Подключение клиента
+    private Thread clientReceiveThread; //Поток
+    private int ipPort = 4042;                      //TCP/IP PORT
+    private string IpAd_loc = "localhost";          //TCP/IP ADRESS
+    public int NumOfConnection = 1;  //Кол-во подклчений
+    private int TCPtimeout = 3; //Тайм-аут для остановки переподключения, при попытке переподключения редактор запускается после остановленной симуляции. Сначала нужно было остановить сервер
+    private string message2send; //Сообщение для отправки
+    public Queue<string> TCP_recv_queue = new Queue<string>(); //Очередь для хранения полученных сообщений
+    private string Rx; //Переменная для передачи данных в Main
+
+
+    ///Старт вызывается перед обновлением первого кадра
+    void Start()
+    {
+        Debug.Log("Соединился.");
+        ConnectToTcpServer(); //Соединияемся
+        
+    }
+
+    ///Обновление вызывается один раз за кадр
+    void Update()
+    {
+        if (TCP_recv_queue.Count > 0) //Если есть какое-то сообщение
+        {
+            string msg = TCP_recv_queue.Dequeue();
+            if ((msg.Contains("O1G") || msg.Contains("G1O")) && msg.Contains("&"))
+            {
+                Rx = msg;
+            }
+        }
+        else
+        {
+            Debug.Log("Que is empty");
+        }
+    }
+
+    /// <summary>
+    /// Функция, которая возвращает сообщения из TCP.cs
+    /// </summary>
+    /// <returns></returns>
+    public string RxMsg() 
+    {
+        return Rx;
+    }
+
+    /// <summary>
+    /// Функция запуска TCP клиента<br></br>
+    /// Так же запускается в отдельном потоке и работает на фоне
+    /// </summary>
+    private void ConnectToTcpServer()
+    {
+        try
+        {
+            clientReceiveThread = new Thread(new ThreadStart(Con));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
+        }
+    }
+
+    /// <summary>
+    /// Функция подключения к серверу, вызываемая из потока
+    /// </summary>
+    private void Con()
+    {
+        bool connect = true;
+        while (connect)
+        {
+            NumOfConnection += 1;
+            if (NumOfConnection < TCPtimeout)
+            {
+                int i = 1;
+                try
+                {
+                    socketConnection = new TcpClient(IpAd_loc, ipPort); //Создаём подключение
+                    message2send = "U3D00"; //Сообщение, которое отправляем на сервер, для старта потока данных
+                    SendMessage(); //Отправляем сообщение
+                    ListenForData(); //
+                    connect = false;
+                    i++;
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                clientReceiveThread.Abort();
+                Debug.Log("Exit the game");
+                Application.Quit();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Функция пересоеденения к серверу
+    /// </summary>
+    private void ReCon()
+    {
+        NumOfConnection += 1;
+        if (NumOfConnection < TCPtimeout)
+        {
+            try
+            {
+                socketConnection = new TcpClient(IpAd_loc, ipPort);
+                message2send = "U3D00";
+                SendMessage();
+                ListenForData();
+            }
+            catch
+            {
+
+            }
+        }
+        else
+        {
+            clientReceiveThread.Abort();
+            Debug.Log("Exit the game");
+            Application.Quit();
+        }
+
+    }
+
+    /// <summary>
+    /// Функция получает данные с сервера и кладёт в очередь
+    /// </summary>
+    private void ListenForData()
+    {
+        try
+        {
+            NumOfConnection = 1;
+            Byte[] bytes = new Byte[32768];
+            while (true)
+            {
+                //Получаем объект потока для чтения 		
+                try
+                {
+                    Debug.Log("connected");
+                    using (NetworkStream stream = socketConnection.GetStream())
+                    {
+                        int length;
+                        TCP_recv_queue.Clear();
+
+                        //Считатываем входящий поток в массив байтов 
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+
+                            //Преобразование массива байтов в строковое сообщение						
+                            string serverMessage = Encoding.ASCII.GetString(incommingData);
+                            TCP_recv_queue.Enqueue(serverMessage);
+                        }
+                    }
+                }
+                catch
+                {
+                    Debug.Log("lost connection");
+                    ReCon();
+                }
+            }
+        }
+        catch
+        {
+            ReCon();
+        }
+    }
+
+
+    /// <summary>
+    /// Отправляет сообщение, хранящиеся в message2send, на сервер
+    /// </summary>
+    private void SendMessage()
+    {
+        if (socketConnection == null)
+        {
+            Debug.Log("NO SERVER AVAILABLE");
+        }
+        try
+        {
+            //Получить объект потока для записи 			
+            NetworkStream stream = socketConnection.GetStream();
+            if (stream.CanWrite)
+            {
+                string clientMessage = message2send;
+                //Преобразование строкового сообщения в массив байтов                
+                byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
+                //Записываем массив байтов в поток socketConnection.                
+                stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Выключаюсь");
+        clientReceiveThread.Abort();
+    }
+}
---
+++ ADS/Assets/Scripts/scr_VehicleHandler.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class scr_VehicleHandler : MonoBehaviour
+{
+    [Header("Колёса")]
+    [SerializeField] private GameObject[] Wheels = new GameObject[4];
+    private float lastRot;
+    private float Rotation;
+
+    [Header("Машина")]
+    public Transform target;
+
+    [Header("Параметры управления")]
+    public float steer_rat = 14.4f;    //steering ratio
+
+    [SerializeField] private float speedmultiplier = 0.1f; 
+    public float steer_correction = 0; //Соответствие между текущим положением рулевого колеса и нулевым углом в градусах
+    private float hdgdif;
+    private float lasthdg;
+    float steerangle;
+    [SerializeField] private float steeringDamping = 4;
+    private float SA_last;
+    private float SA_wanted;
+    private float SA_calc;
+
+
+    [Header("BrakeLight Objects")]
+    [SerializeField] private GameObject[] BrakeLight = new GameObject[2];
+
+    private void Start()
+    {
+        SA_last = 0;
+        SA_wanted = 0;
+        lasthdg = 0;
+
+        Rotation = 0;
+        lastRot = 0;
+    }
+
+    public void CalculateSteering(float heading, float speed, float timer)
+    {
+        hdgdif = heading - lasthdg; //Разница между текущим heading и взвешенными последними 3 heading
+        lasthdg = heading;
+        SA_wanted =  hdgdif * steer_rat;
+        SA_calc = Mathf.LerpAngle(SA_last, SA_wanted, steeringDamping * Time.deltaTime);
+        SA_last = SA_calc;
+        WheelHandler(SA_calc, heading, speed, timer);
+    }
+
+    public void WheelHandler(float SteerAngle, float heading, float speed, float timer) //Другие автомобили
+    {
+        Rotation = lastRot + speed*(speedmultiplier * timer * (1/3.6f)); //Вычисление вращения колес на основе скорости и времени
+        if (Rotation > 360)
+        {
+            Rotation = Rotation - 360;
+        }
+        else if (Rotation < 0)
+        {
+            Rotation = Rotation + 360;
+        }
+        Wheels[0].transform.rotation = Quaternion.Euler((Rotation), (SteerAngle) + heading, 0f);
+        Wheels[1].transform.rotation = Quaternion.Euler((Rotation), (SteerAngle) + heading, 0f);
+        Wheels[2].transform.rotation = Quaternion.Euler((Rotation), heading, 0f);
+        Wheels[3].transform.rotation = Quaternion.Euler((Rotation), heading, 0f);
+        lastRot = Rotation;
+    }
+
+    public void BrakeLightSwitch(bool state)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            Light light = (Light)BrakeLight[i].GetComponent("Light");    //Поворотники
+            if (state)
+            {
+                if (light.intensity < 20)
+                    light.intensity += 5;
+            }
+            else
+            {
+                if (light.intensity > 0)
+                    light.intensity -= 1;
+            }
+        }        
+    }
+}
