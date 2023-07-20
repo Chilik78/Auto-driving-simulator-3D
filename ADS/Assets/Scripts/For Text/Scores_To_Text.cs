67da6e2e7dc9f1266650faffffdb5dd1e3ba5d29
Chilik78 <104494266+Chilik78@users.noreply.github.com>
2023-07-20 23:00:46 +0300

    Отнимание штрафов

--- ADS/Assets/Scripts/For Text/Scores_To_Text.cs
+++ ADS/Assets/Scripts/For Text/Scores_To_Text.cs
@@ -6,43 +6,17 @@ using UnityEngine.UI;
 
 public class Scores_To_Text : MonoBehaviour
 {
-    public Text visualSubScores;
-    public Canvas currentCanvas;
-    private int lastScores = 100;
     public Text scoresText;
+    public Text visualSubScores;
+    private int lastScores = 100;
+    
 
-    private Vector3 startPos;
-    private Vector3 endPos;
-    private float desiredDuration = 3f;
-    private float elapsedTime = 0;
-    private bool isMoving = false;
-
+    private int deltaScores;
     void Update()
     {
-
-        /*if (*//*lastScores != Penalty.GetScores() &&*//* !isMoving && Input.GetKeyDown(KeyCode.H))
-        {
-
-
-            isMoving = true;
-
-            if(scoresText.transform.childCount < 1)
-            {
-                Instantiate(visualSubScores, currentCanvas.transform);
-                visualSubScores.text = $"{Penalty.GetScores() - lastScores}";
-                visualSubScores.rectTransform.position = new Vector3(0, 0, 0);
-                startPos = scoresText.rectTransform.position;
-                endPos = new Vector3(visualSubScores.rectTransform.position.x + 100, visualSubScores.rectTransform.position.y, visualSubScores.rectTransform.position.z);
-            }
-
-            StartCoroutine(MovingSubstraction());
-            lastScores = Penalty.GetScores();
-            scoresText.text = "Очки: " + lastScores;
-        }*/
-
         if (lastScores != Penalty.GetScores() && !visualSubScores.GetComponent<Text>().enabled)
         {
-            Debug.Log("Zachel");
+            deltaScores = Penalty.GetScores() - lastScores;
             lastScores = Penalty.GetScores();
             scoresText.text = "Очки: " + lastScores;
             StartCoroutine(MoveText());
@@ -51,37 +25,11 @@ public class Scores_To_Text : MonoBehaviour
 
     IEnumerator MoveText()
     {
+        visualSubScores.GetComponent<Animator>().enabled = true;
         visualSubScores.GetComponent<Text>().enabled = true;
-        yield return new WaitForSeconds(0.3f);
+        visualSubScores.GetComponent<Text>().text = deltaScores.ToString();
+        yield return new WaitForSeconds(0.5f);
         visualSubScores.GetComponent<Text>().enabled = false;
-    }
-
-    private IEnumerator MovingSubstraction()
-    {
-        isMoving = true;
-
-  
-
-        yield return new WaitForSeconds(1.0f);
-
-        Debug.Log("Zachel v Coroutine");
-
-        if (isMoving)
-        {
-            elapsedTime += Time.deltaTime;
-
-            visualSubScores.rectTransform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0, 1, elapsedTime));
-
-            if (visualSubScores.rectTransform.position == endPos)
-            {
-                Debug.Log("END");
-                elapsedTime = 0;
-                Destroy(visualSubScores);
-                StopAllCoroutines();
-                isMoving = false;
-            }
-
-            StartCoroutine(MovingSubstraction());
-        }
+        visualSubScores.GetComponent<Animator>().enabled = false;
     }
 }
